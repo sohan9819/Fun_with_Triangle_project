941317dbc0b323800d2a95f601e70d55c8af9da0
sohan9819 <sohanshetty2001@gmail.com>
2021-09-09 00:26:09 +0530

    isTriangle Js completed

---
+++ isTraingle/app.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('#form')
+const result = document.querySelector('#result')
+
+form.addEventListener('submit' , function(e){
+    e.preventDefault()
+    if ( this.angle1.value === "" || this.angle2.value === "" || this.angle3.value === "" ){
+        result.innerText = "Enter all the angles "
+    }
+    else {
+        const a = parseInt(this.angle1.value)
+        const b = parseInt(this.angle2.value)
+        const c = parseInt(this.angle3.value)
+    
+        console.log(a+b+c)
+    
+        checkTraingle(a,b,c)
+    }
+})
+
+const checkTraingle = (a,b,c) => {
+    if ( a + b + c === 180){
+        result.innerText = "Yay, the angles form a triangle!"
+    } else {
+        result.innerText = "Oh Oh! The angle doesn't form a triangle"
+    }
+}
